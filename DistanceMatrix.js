57dc79e0513379a54d53b33b2c6bbfb56309dcc5
Tom Kirchgäßner <tomkirchgaessner@Toms-MBP.fritz.box>
2017-02-18 18:30:46 +0100

    added driving and fixed marker-bug

--- DistanceMatrix.js
+++ DistanceMatrix.js
@@ -1,17 +1,7 @@
-
-/*
-KEIN EXTRA OPTIONS OBJEKT- da kann man google methoden nicht verwenden, weil sie noch bnicht geladen sind!
-
-2017-02-09 Erster laufender Built. To Do: Marker nach jedem geocoding löschen, code aufräumen, autofahrzeit hinzufügen, STYLING!!
-
-2017-02-12 HTML Styling.
-
-*/
-
-//map init
-
 var map;
 var RowCounter = 0;
+var markersArray = [];
+
 
 window.initMap = function()
 {
@@ -19,8 +9,8 @@ window.initMap = function()
 
   var mapOptions =
   {
-    center: new google.maps.LatLng(48.153403, 11.529496), //münchen
-    zoom: 12
+    center: new google.maps.LatLng(51.315875, 9.494597), //münchen
+    zoom: 5
   };
   map = new google.maps.Map(document.getElementById('map'), mapOptions); //bekommt den Div in der HTMl und Optionen
 
@@ -36,7 +26,7 @@ window.initMap = function()
   (
     'click', function()
     {
-
+    deleteMarkers(markersArray);
     geocodeAddress(geocoder, map);
     }
   );
@@ -55,10 +45,15 @@ window.initMap = function()
 //geocoder, nimmt den wert aus der origin textbox und geocodiert ihn
 function geocodeAddress(geocoder, resultsMap)
 {
+  console.log('clearmarkers');
+  for (var i = 0; i < markersArray.length; i++) {
+    markersArray[i].setMap(null);
+  };
+
   console.log('Starte function geocodeAdress');
 
   var origin = document.getElementById('origin').value;
-  var markersArray = [];
+
 
   geocoder.geocode({'address': origin}, function(results, status)
   {
@@ -69,14 +64,9 @@ function geocodeAddress(geocoder, resultsMap)
         //map zentrieren und marker auf die origin setzen
 
         resultsMap.setCenter(results[0].geometry.location);
-        console.log('Map wird gecentert');
-        var marker = new google.maps.Marker
-        ({
-          map: resultsMap,
-          position: results[0].geometry.location
-        });
+        console.log('map zentrieren und marker auf die origin setzen');
 
-        //erstmal Destinations anlegen
+    //erstmal Destinations anlegen
         console.log('Destination Array wird angelegt');
         var destinations = new Array();
         for (k=0; k<RowCounter; k++)
@@ -137,7 +127,7 @@ function geocodeAddress(geocoder, resultsMap)
               var destinationList = response.destinationAddresses;
               var outputDiv = document.getElementById('Transit');
               outputDiv.innerHTML = '';
-              deleteMarkers(markersArray);
+              //deleteMarkers(markersArray);
 
               var showGeocodedAddressOnMap = function(asDestination) {
                 var icon = asDestination ? destinationIcon : originIcon;
@@ -229,6 +219,61 @@ function geocodeAddress(geocoder, resultsMap)
                       }
                     }) //Bike
 
+                    //Car
+                    console.log('Beginne mit getDistanceMatrix Driving');
+                    service.getDistanceMatrix({
+                      origins: [origin],
+                      destinations: destinationsLatLng,
+                      travelMode: google.maps.TravelMode.DRIVING,
+                      transitOptions: {  departureTime: afternoon },
+                      unitSystem: google.maps.UnitSystem.METRIC,
+                      avoidHighways: false,
+                      avoidTolls: false
+                    }, function(response, status) {
+                      if (status !== google.maps.DistanceMatrixStatus.OK) {
+                        alert('Error was: ' + status);
+                      } else {
+                          var originList = response.originAddresses;
+                          var destinationList = response.destinationAddresses;
+                          var outputDiv = document.getElementById('Driving');
+                          outputDiv.innerHTML = '';
+                          //deleteMarkers(markersArray);
+
+                          var showGeocodedAddressOnMap = function(asDestination) {
+                            var icon = asDestination ? destinationIcon : originIcon;
+                            return function(results, status) {
+                              if (status === google.maps.GeocoderStatus.OK) {
+                                map.fitBounds(bounds.extend(results[0].geometry.location));
+                                if (map.getZoom()>12) {
+                                  map.setZoom(12);
+                                };
+                                markersArray.push(new google.maps.Marker({
+                                  map: map,
+                                  position: results[0].geometry.location,
+                                  icon: destinationIcon
+                                }));
+                              }
+                            };
+                          };
+
+
+
+                          for (var i = 0; i < originList.length; i++) {
+                            var results = response.rows[i].elements;
+                            geocoder.geocode({'address': originList[i.LatLng]},
+                                showGeocodedAddressOnMap(false));
+                            for (var j = 0; j < results.length; j++) {
+                              geocoder.geocode({'address': destinations[j][1]},
+                                  showGeocodedAddressOnMap(true));
+                              outputDiv.innerHTML += '<br>' + "<span style='font-family: Ubuntu; color:#FFFFFF; padding: 0px;'>"  + 'Zu '+ destinations[j][0] +
+                                  ': ' + results[j].distance.text + ' in ' +
+                                  "<span style='color:#FFA13E;'>" + results[j].duration.text + "</span>" + "</span>";
+
+                                }
+                              }
+                            }
+                          }) //Walking
+
                     //Walking
                     console.log('Beginne mit getDistanceMatrix Walking');
                     service.getDistanceMatrix({
@@ -296,6 +341,8 @@ function deleteMarkers(markersArray) {
   }
   markersArray = [];
 }
+
+
 
 function addRow(Table) //jede Zelle wird mit Matrizenindizes in der ID benannt
 {
